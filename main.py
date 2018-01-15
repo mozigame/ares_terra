e9333514c6f5245822600822202422f6ca365203
joey <joey12492@gmail.com>
2018-01-15 21:12:12 +0800

    first push

---
+++ main.py
@@ -0,0 +1,15 @@
+from  com.bc.cp.util.redis_util import *
+import sys
+
+if __name__ == '__main__':
+
+    # exe_pipeline_del_redis_keys('_bet_userOrStatusSet*')
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'delete':
+            exe_pipeline_del_redis_keys(sys.argv[2] + "*")
+        elif sys.argv[1] == 'get':
+            exe_keys(sys.argv[2] + "*")
+        else:
+            print('use valid command')
+    else:
+        print('no redis match key')
